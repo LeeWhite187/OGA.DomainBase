b2c95ad6ecfbb6ad8fc3ab5017c8a50671f1e3ac
LeeWhite187 <LeeWhite187@gmail.com>
2023-04-09 15:41:57 -0400

    Made a permanent home for the ServiceBase class that has existed in the MOVEELSEWHERE library copies. It now resides in this library. +semver:patch

---
+++ OGA.DomainBase/OGA.DomainBase_SP/Services/ServiceBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OGA.DomainBase.Services
+{
+    public interface IDomainServiceBase<TId>
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Passed in at construction, to give the domain service a shared context for logging and event population.
+        /// </summary>
+        public string CorelationId { get; set; }
+
+        public OGA.DomainBase.Models.UserContext<TId> UserContext { get; set; }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// Provides common data and methods used by domain-level services, such as User context, corelationId, etc...
+    /// </summary>
+    public class DomainServiceBase<TId> : IDomainServiceBase<TId>
+    {
+        #region Private Fields
+
+        static protected string _classname = nameof(DomainServiceBase<TId>);
+
+        #endregion
+
+
+        #region Public Properties
+
+        /// <summary>
+        /// Passed in at construction, to give the domain service a shared context for logging and event population.
+        /// </summary>
+        public string CorelationId { get; set; }
+
+        public OGA.DomainBase.Models.UserContext<TId> UserContext { get; set; }
+
+        #endregion
+
+        public DomainServiceBase()
+        {
+            CorelationId = "";
+            UserContext = new OGA.DomainBase.Models.UserContext<TId>();
+        }
+    }
+}
