2fa7e3e1d37fe917e1346bd8947bc6bbef1f8ef8
LeeWhite187 <LeeWhite187@gmail.com>
2024-03-27 20:28:41 -0400

    Updated library dependencies. Generalized runtime identifier to apply to all Win and all Linux runtimes. +semver:minor

--- OGA.DomainBase/buildpublish.bat
+++ OGA.DomainBase/buildpublish.bat
@@ -2,22 +2,22 @@ REM NET Core Software Library
 
 REM Build the library...
 dotnet restore "./OGA.DomainBase_NET5/OGA.DomainBase_NET5.csproj"
-dotnet build "./OGA.DomainBase_NET5/OGA.DomainBase_NET5.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+dotnet build "./OGA.DomainBase_NET5/OGA.DomainBase_NET5.csproj" -c DebugLinux --runtime linuxs --no-self-contained
 
 dotnet restore "./OGA.DomainBase_NET5/OGA.DomainBase_NET5.csproj"
-dotnet build "./OGA.DomainBase_NET5/OGA.DomainBase_NET5.csproj" -c DebugWin --runtime win-x64 --no-self-contained
+dotnet build "./OGA.DomainBase_NET5/OGA.DomainBase_NET5.csproj" -c DebugWin --runtime win --no-self-contained
 
 dotnet restore "./OGA.DomainBase_NET6/OGA.DomainBase_NET6.csproj"
-dotnet build "./OGA.DomainBase_NET6/OGA.DomainBase_NET6.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+dotnet build "./OGA.DomainBase_NET6/OGA.DomainBase_NET6.csproj" -c DebugLinux --runtime linux --no-self-contained
 
 dotnet restore "./OGA.DomainBase_NET6/OGA.DomainBase_NET6.csproj"
-dotnet build "./OGA.DomainBase_NET6/OGA.DomainBase_NET6.csproj" -c DebugWin --runtime win-x64 --no-self-contained
+dotnet build "./OGA.DomainBase_NET6/OGA.DomainBase_NET6.csproj" -c DebugWin --runtime win --no-self-contained
 
 dotnet restore "./OGA.DomainBase_NET7/OGA.DomainBase_NET7.csproj"
-dotnet build "./OGA.DomainBase_NET7/OGA.DomainBase_NET7.csproj" -c DebugLinux --runtime linux-x64 --no-self-contained
+dotnet build "./OGA.DomainBase_NET7/OGA.DomainBase_NET7.csproj" -c DebugLinux --runtime linux --no-self-contained
 
 dotnet restore "./OGA.DomainBase_NET7/OGA.DomainBase_NET7.csproj"
-dotnet build "./OGA.DomainBase_NET7/OGA.DomainBase_NET7.csproj" -c DebugWin --runtime win-x64 --no-self-contained
+dotnet build "./OGA.DomainBase_NET7/OGA.DomainBase_NET7.csproj" -c DebugWin --runtime win --no-self-contained
 
 REM Create the composite nuget package file from built libraries...
 C:\Programs\nuget\nuget.exe pack ./OGA.DomainBase.nuspec -IncludeReferencedProjects -symbols -SymbolPackageFormat snupkg -OutputDirectory ./Publish -Verbosity detailed
